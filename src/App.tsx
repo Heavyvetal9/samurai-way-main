52b02049e42e536438944dfdd8e8988c0bcdc267
HeavyVetal9 <buryanov9@yandex.ru>
2024-01-21 15:22:42 +0300

    samurai-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {FullInput} from "./components/FullInput";
 
 function App() {
+    let [message, setMessage] = useState(
+        [
+            {message: "message1"},
+            {message: "message2"},
+            {message: "message3"}
+        ]
+    )
+
+    const addMessage=(title: string)=>{
+        let newMessage={message:title};
+        setMessage([newMessage,...message])
+    }
+
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <div>
+         <FullInput addMessage={addMessage}/>
+
+     </div>
+        {message.map((el,index) => {
+            return(
+                <div key={index}>{el.message}</div>
+            )
+            }
+        )
+        }
     </div>
   );
 }
